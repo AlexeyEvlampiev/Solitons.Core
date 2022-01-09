d621a9f51d855f69921aa24287464e883b391951
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2022-01-09 19:22:56 +0000

    Refactoring

--- test/Solitons.Azure.XUnitTest/Host.cs
+++ test/Solitons.Azure.XUnitTest/Host.cs
@@ -5,13 +5,14 @@ namespace Solitons.Azure
 {
     public static class Host
     {
+        private const string StorageConnectionStringKey = "AZ_STORAGE_CONNECTION_STRING";
         private static  readonly Lazy<string> LazyStorageConnectionString;
 
         static Host()
         {
             LazyStorageConnectionString = new Lazy<string>(()=> Environment
-                .GetEnvironmentVariable("AZ_STORAGE_CONNECTION_STRING")
-                .DefaultIfNullOrWhiteSpace("UseDevelopmentStorage=true"));
+                .GetEnvironmentVariable(StorageConnectionStringKey)
+                .ThrowIfNullOrWhiteSpace(()=> new InvalidOperationException($"{StorageConnectionStringKey} environment variable is missing.")));
         }
 
         public static string StorageConnectionString
