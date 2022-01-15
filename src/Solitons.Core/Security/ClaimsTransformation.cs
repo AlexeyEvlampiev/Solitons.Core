333f63ec8c62646243da40c670cebbe511beeca2
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2022-01-15 21:44:06 +0100

    wip

---
+++ src/Solitons.Core/Security/ClaimsTransformation.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Solitons.Security
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class ClaimsTransformation
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        protected abstract Task AlterAsync(ClaimsPrincipal principal);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="principal"></param>
+        /// <returns></returns>
+        public async Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            principal = principal?.Clone() ?? new ClaimsPrincipal();
+            await AlterAsync(principal);
+            return principal;
+        }
+
+    }
+}
