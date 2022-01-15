271445b4b9d696af950c68f5fc51025fa279a4b2
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2022-01-15 18:58:24 +0100

    wip

--- src/Solitons.Core/Collections/CollectionProxy.cs
+++ src/Solitons.Core/Collections/CollectionProxy.cs
@@ -5,56 +5,129 @@ using System.Diagnostics;
 
 namespace Solitons.Collections
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public abstract class CollectionProxy<T> : ICollection<T>
     {
         [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
         private readonly ICollection<T> _innerCollection;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="innerCollection"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         [DebuggerStepThrough]
         protected CollectionProxy(ICollection<T> innerCollection)
         {
             _innerCollection = innerCollection ?? throw new ArgumentNullException(nameof(innerCollection));
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public IEnumerator<T> GetEnumerator() => _innerCollection.GetEnumerator();
+
 
         [DebuggerStepThrough]
         IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable) _innerCollection).GetEnumerator();
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
-        public virtual void Add(T item) => _innerCollection.Add(item);
+        public virtual CollectionProxy<T> Add(T item)
+        {
+            _innerCollection.Add(item);
+            return this;
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [DebuggerStepThrough]
-        public virtual void Clear() => _innerCollection.Clear();
+        public virtual CollectionProxy<T> Clear()
+        {
+            _innerCollection.Clear();
+            return this;
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public bool Contains(T item) => _innerCollection.Contains(item);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
         [DebuggerStepThrough]
         public void CopyTo(T[] array, int arrayIndex) => _innerCollection.CopyTo(array, arrayIndex);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public virtual bool Remove(T item) => _innerCollection.Remove(item);
 
+        /// <summary>
+        /// 
+        /// </summary>
         public int Count
         {
             [DebuggerStepThrough]
             get => _innerCollection.Count;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public bool IsReadOnly 
         {
             [DebuggerStepThrough]
             get => _innerCollection.IsReadOnly;
         }
 
+
+        [DebuggerStepThrough]
+        void ICollection<T>.Add(T item) => _innerCollection.Add(item);
+
+        [DebuggerStepThrough]
+        void ICollection<T>.Clear() => _innerCollection.Clear();
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public override string ToString() => _innerCollection.ToString();
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public override bool Equals(object? obj) => _innerCollection.Equals(obj);
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         [DebuggerStepThrough]
         public override int GetHashCode() => _innerCollection.GetHashCode();
     }
---
+++ src/Solitons.Core/Collections/FluentList.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Solitons.Collections
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class FluentList<T> : IList<T>
+    {
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        private readonly List<T> _list;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DebuggerNonUserCode]
+        public FluentList()
+        {
+            _list = new List<T>();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="capacity"></param>
+        [DebuggerNonUserCode]
+        public FluentList(int capacity)
+        {
+            _list = new List<T>(capacity);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [DebuggerNonUserCode]
+        public FluentList(IEnumerable<T> collection)
+        {
+            _list = new List<T>(collection);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fluentList"></param>
+        [DebuggerNonUserCode]
+        public static explicit operator List<T>(FluentList<T> fluentList) => fluentList?._list;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="list"></param>
+        [DebuggerNonUserCode]
+        public static explicit operator FluentList<T>(List<T> list) => list == null ? null : new FluentList<T>();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public IEnumerator<T> GetEnumerator() => _list.GetEnumerator();
+
+        [DebuggerNonUserCode]
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_list).GetEnumerator();
+
+        [DebuggerNonUserCode]
+        void ICollection<T>.Add(T item) => _list.Add(item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> Add(T item)
+        {
+            _list.Add(item);
+            return this;
+        }
+
+        [DebuggerNonUserCode]
+        void ICollection<T>.Clear() => _list.Clear();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> Clear()
+        {
+            _list.Clear();
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public bool Contains(T item) => _list.Contains(item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        [DebuggerNonUserCode]
+        public void CopyTo(T[] array, int arrayIndex) => _list.CopyTo(array, arrayIndex);
+
+        [DebuggerNonUserCode]
+        bool ICollection<T>.Remove(T item) => _list.Remove(item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> Remove(T item)
+        {
+            _list.Remove(item);
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <param name="removed"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> Remove(T item, out bool removed)
+        {
+            removed = _list.Remove(item);
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [DebuggerNonUserCode]
+        public int Count => _list.Count;
+
+        [DebuggerNonUserCode]
+        bool ICollection<T>.IsReadOnly => ((ICollection<T>)_list).IsReadOnly;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public int IndexOf(T item) => _list.IndexOf(item);
+
+        [DebuggerNonUserCode]
+        void IList<T>.Insert(int index, T item) => _list.Insert(index, item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> Insert(int index, T item)
+        {
+            _list.Insert(index, item);
+            return this;
+        }
+
+        [DebuggerNonUserCode]
+        void IList<T>.RemoveAt(int index) => _list.RemoveAt(index);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        [DebuggerNonUserCode]
+        public FluentList<T> RemoveAt(int index)
+        {
+            _list.RemoveAt(index);
+            return this;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public T this[int index]
+        {
+            [DebuggerNonUserCode]
+            get => _list[index];
+            [DebuggerNonUserCode]
+            set => _list[index] = value;
+        }
+    }
+}
