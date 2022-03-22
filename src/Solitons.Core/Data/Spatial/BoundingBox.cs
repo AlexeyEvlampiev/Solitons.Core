f60c4ef0f4a6b0ded30dbf4978161cba1c0006f9
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2022-03-22 11:06:57 +0100

    Squashed commit of the following:
    commit 17649a39ad043552935ec97113b62c7667cbbab4
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Tue Mar 22 11:06:25 2022 +0100
    
        wip

---
+++ src/Solitons.Core/Data/Spatial/BoundingBox.cs
@@ -0,0 +1,113 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Solitons.Data.Spatial
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public struct BoundingBox
+    {
+        private const string InvalidCsvFormatMessage = "Invalid bounding box CSV format";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xmin"></param>
+        /// <param name="ymin"></param>
+        /// <param name="xmax"></param>
+        /// <param name="ymax"></param>
+        public BoundingBox(float xmin, float ymin, float xmax, float ymax)
+        {
+            Xmin = xmin;
+            Ymin = ymin;
+            Xmax = xmax;
+            Ymax = ymax;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="csv"></param>
+        /// <returns></returns>
+        /// <exception cref="FormatException"></exception>
+        [DebuggerStepThrough]
+        public static BoundingBox ParseCsv(string csv)
+        {
+            return TryParseCsv(csv, out var boundingBox)
+                ? boundingBox
+                : throw new FormatException(InvalidCsvFormatMessage);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="csv"></param>
+        /// <param name="boundingBox"></param>
+        /// <returns></returns>
+        public static bool TryParseCsv(string csv, out BoundingBox boundingBox)
+        {
+            var parts = Regex
+                .Split(csv, @"\s*,\s*")
+                .Skip(String.IsNullOrWhiteSpace);
+            var coordinates = new List<float>(4);
+            foreach (var part in parts)
+            {
+                if (float.TryParse(part, NumberStyles.Float, CultureInfo.InvariantCulture, out var coordinate) &&
+                    coordinates.Count < 4)
+                {
+                    coordinates.Add(coordinate);
+                }
+                else
+                {
+                    boundingBox = default;
+                    return false;
+                }
+            }
+
+            if (coordinates.Count == 4)
+            {
+                boundingBox = new BoundingBox(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+                return true;
+            }
+
+            boundingBox = default;
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="boundingBox"></param>
+        /// <returns></returns>
+        public static implicit operator float[](BoundingBox boundingBox)
+        {
+            return new float[] { boundingBox.Xmin, boundingBox.Ymin, boundingBox.Xmax, boundingBox.Ymax};
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Xmin { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Ymin { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Xmax { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public float Ymax { get; }
+    }
+}
---
+++ test/Solitons.Core.XUnitTest/Data/Spatial/BoundingBox_TryParseCsv_Should.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Solitons.Data.Spatial
+{
+    // ReSharper disable once InconsistentNaming
+    public class BoundingBox_TryParseCsv_Should
+    {
+        [Theory]
+        [InlineData("149488.29419188126,410950.33041645837,149537.4305324534,411007.5940121479", 
+            149488.29419188126, 410950.33041645837, 149537.4305324534, 411007.5940121479)]
+        public void HandleValidCases(string csv, float xmin, float ymin, float xmax, float ymax)
+        {
+            Assert.True(BoundingBox.TryParseCsv(csv, out var boundingBox));
+            Assert.Equal(xmin, boundingBox.Xmin);
+            Assert.Equal(ymin, boundingBox.Ymin);
+            Assert.Equal(xmax, boundingBox.Xmax);
+            Assert.Equal(ymax, boundingBox.Ymax);
+        }
+    }
+}
