c0ba40f0f0b785c46370f8b25c9a7b5d1e9096a1
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2022-04-15 20:21:49 +0200

    Squashed commit of the following:
    commit 97cefeb3473dd3af2538174199aaa150d564af38
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Apr 15 20:20:36 2022 +0200
    
        wip

---
+++ src/Solitons.Core/Security/Common/JsonToken.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+
+namespace Solitons.Security.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class JsonToken : IEquatable<JsonToken>
+    {
+        private readonly IEncoder _encoder;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="encoder"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        protected JsonToken(IEncoder encoder)
+        {
+            _encoder = encoder ?? throw new ArgumentNullException(nameof(encoder));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected JsonToken() : this(new Utf8Encoder())
+        {
+            
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public sealed override string ToString()
+        {
+            var json = JsonSerializer.Serialize(this, GetType());
+            return _encoder.Encode(json).ToBase64String();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="input"></param>
+        /// <param name="encoder"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FormatException"></exception>
+        [DebuggerStepThrough]
+        protected static T Parse<T>(string input, IEncoder? encoder = null) where T : JsonToken, new()
+        {
+            input = input.ThrowIfNullOrWhiteSpaceArgument(nameof(input));
+            encoder ??= new Utf8Encoder();
+
+            var bytes = input
+                .ThrowIfNullOrWhiteSpaceArgument(nameof(input))
+                .AsBase64Bytes();
+            var json = encoder.Decode(bytes);
+            return JsonSerializer.Deserialize<T>(json) 
+                   ?? throw new FormatException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected interface IEncoder
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="json"></param>
+            /// <returns></returns>
+            byte[] Encode(string json);
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="bytes"></param>
+            /// <returns></returns>
+            string Decode(byte[] bytes);
+        }
+
+        sealed class Utf8Encoder : IEncoder
+        {
+            public byte[] Encode(string json) => Encoding.UTF8.GetBytes(json);
+
+            public string Decode(byte[] bytes) => Encoding.UTF8.GetString(bytes);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(JsonToken? other)
+        {
+            if (other is null) return false;
+            if (other.GetType() != this.GetType()) return false;
+            return StringComparer.Ordinal.Equals(this.ToString(), other.ToString());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((JsonToken)obj);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode() => ToString().GetHashCode();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator ==(JsonToken? left, JsonToken? right) => Equals(left, right);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static bool operator !=(JsonToken? left, JsonToken? right) => !Equals(left, right);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="token"></param>
+        public static implicit operator string?(JsonToken? token) => token?.ToString();
+    }
+}
---
+++ test/Solitons.Core.XUnitTest/Security/Common/JsonToken_ToString_Should.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Xunit;
+
+namespace Solitons.Security.Common
+{
+
+    // ReSharper disable once InconsistentNaming
+    public sealed class JsonToken_ToString_Should
+    {
+        [Fact]
+        public void Work()
+        {
+            var token = new TestToken { Id = Guid.Parse("500b0833-94ba-4670-9adb-45277cc25e3e") };
+
+            var tokenString = token.ToString();
+            var clone = TestToken.Parse(tokenString);
+            Assert.Equal(token.Id, clone.Id);
+            Assert.Equal(token, clone);
+        }
+
+        public sealed class TestToken : JsonToken
+        {
+            [JsonPropertyName("uuid")]
+            public Guid Id { get; set; }
+
+            public static TestToken Parse(string input) => Parse<TestToken>(input);
+        }
+    }
+}
