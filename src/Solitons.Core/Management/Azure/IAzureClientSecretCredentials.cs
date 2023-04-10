786ed4bdc6097ccbc36fd787c0b4105b118289b0
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2023-04-10 14:37:38 +0200

    Squashed commit of the following:
    commit 74216fc9a29ec7cefe34f60b9f1db9a631983b5c
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Mon Apr 10 14:37:05 2023 +0200
    
        wip

---
+++ src/Solitons.Core/Management/Azure/IAzureClientSecretCredentials.cs
@@ -0,0 +1,26 @@
+﻿namespace Solitons.Management.Azure;
+
+/// <summary>
+/// Provides the credentials required to authenticate with an Azure service using a client secret or certificate.
+/// </summary>
+public interface IAzureClientSecretCredentials
+{
+    /// <summary>
+    /// Gets the Azure Active Directory tenant ID associated with the service.
+    /// </summary>
+    string TenantId { get; }
+
+    /// <summary>
+    /// Gets the client ID associated with the service.
+    /// </summary>
+    string ClientId { get; }
+
+    /// <summary>
+    /// Gets the client secret used to authenticate with the service.
+    /// </summary>
+    /// <remarks>
+    /// If using a client secret, this property should contain the secret key. If using a certificate, this property should
+    /// contain the certificate in either PFX or PEM format.
+    /// </remarks>
+    string ClientSecret { get; }
+}
