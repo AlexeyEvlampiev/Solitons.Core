1329e79180e156b711c09faeeb6b6e393c72aa28
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2023-04-19 12:42:15 +0200

    wip

--- src/Solitons.Core/Data/IDatabaseRpcProvider.cs
+++ src/Solitons.Core/Data/IDatabaseRpcProvider.cs
@@ -2,74 +2,71 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Solitons.Data
+namespace Solitons.Data;
+
+/// <summary>
+/// Provides an interface for communicating with a database via remote procedure call (RPC).
+/// </summary>
+public interface IDatabaseRpcProvider
 {
     /// <summary>
-    /// 
+    /// Sends a request to the database via RPC and returns the response.
     /// </summary>
-    public  interface IDatabaseRpcProvider
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="metadata"></param>
-        /// <param name="request"></param>
-        /// <param name="parseResponse"></param>
-        /// <param name="cancellation"></param>
-        /// <returns></returns>
-        Task<T> InvokeAsync<T>(
-            DatabaseRpcCommandMetadata metadata, 
-            string request, 
-            Func<string, Task<T>> parseResponse, 
-            CancellationToken cancellation = default);
+    /// <typeparam name="T">The type of the response.</typeparam>
+    /// <param name="metadata">The metadata for the RPC command.</param>
+    /// <param name="request">The request string to send.</param>
+    /// <param name="parseResponse">The function used to parse the response string.</param>
+    /// <param name="cancellation">The optional cancellation token.</param>
+    /// <returns>The response of type T.</returns>
+    Task<T> InvokeAsync<T>(
+        DatabaseRpcCommandMetadata metadata, 
+        string request, 
+        Func<string, Task<T>> parseResponse, 
+        CancellationToken cancellation = default);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="metadata"></param>
-        /// <param name="request"></param>
-        /// <param name="parseResponse"></param>
-        /// <param name="cancellation"></param>
-        /// <returns></returns>
-        Task<T> WhatIfAsync<T>(
-            DatabaseRpcCommandMetadata metadata,
-            string request,
-            Func<string, Task<T>> parseResponse,
-            CancellationToken cancellation = default);
+    /// <summary>
+    /// Sends a request to the database via RPC and returns the response without committing the transaction.
+    /// </summary>
+    /// <typeparam name="T">The type of the response.</typeparam>
+    /// <param name="metadata">The metadata for the RPC command.</param>
+    /// <param name="request">The request string to send.</param>
+    /// <param name="parseResponse">The function used to parse the response string.</param>
+    /// <param name="cancellation">The optional cancellation token.</param>
+    /// <returns>The response of type T.</returns>
+    Task<T> WhatIfAsync<T>(
+        DatabaseRpcCommandMetadata metadata,
+        string request,
+        Func<string, Task<T>> parseResponse,
+        CancellationToken cancellation = default);
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="metadata"></param>
-        /// <param name="request"></param>
-        /// <param name="callback"></param>
-        /// <param name="cancellation"></param>
-        /// <returns></returns>
-        Task SendAsync(
-            DatabaseRpcCommandMetadata metadata, 
-            string request, 
-            Func<Task> callback, 
-            CancellationToken cancellation = default);
+    /// <summary>
+    /// Sends a request to the database via RPC and calls the specified callback function once the request has been sent.
+    /// </summary>
+    /// <param name="metadata">The metadata for the RPC command.</param>
+    /// <param name="request">The request string to send.</param>
+    /// <param name="callback">The function to call once the request has been sent.</param>
+    /// <param name="cancellation">The optional cancellation token.</param>
+    Task SendAsync(
+        DatabaseRpcCommandMetadata metadata, 
+        string request, 
+        Func<Task> callback, 
+        CancellationToken cancellation = default);
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="queueName"></param>
-        /// <param name="cancellation"></param>
-        /// <returns></returns>
-        Task ProcessQueueAsync(
-            string queueName, 
-            CancellationToken cancellation = default);
+    /// <summary>
+    /// Processes a queue of requests sent to the database using the <see cref="SendAsync"/> method.
+    /// </summary>
+    /// <param name="queueName">The name of the queue to process.</param>
+    /// <param name="cancellation">The optional cancellation token.</param>
+    Task ProcessQueueAsync(
+        string queueName, 
+        CancellationToken cancellation = default);
 
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="callback"></param>
-        /// <returns></returns>
-        IDatabaseRpcProvider With(IDatabaseRpcProviderCallback callback);
-    }
+    /// <summary>
+    /// Adds a callback to the database RPC provider.
+    /// </summary>
+    /// <param name="callback">The callback to add.</param>
+    /// <returns>The modified IDatabaseRpcProvider object.</returns>
+    IDatabaseRpcProvider With(IDatabaseRpcProviderCallback callback);
 }
