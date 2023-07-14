be2ce4c9fb9e458e37f32bc72239601b772a38a8
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2023-07-14 23:24:33 +0200

    Squashed commit of the following:
    commit f3e38c7f0a67c43a38370a8a40eb876cd3f8ed9e
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 23:24:04 2023 +0200
    
        wip
    
    commit 171f07452cd3e1d02a17ff385c52cb06340167e8
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 20:05:55 2023 +0200
    
        wip
    
    commit 40557acb7e92112a99968cb348244e7e2df19b8b
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 19:51:25 2023 +0200
    
        wip
    
    commit f20f9154dfed2423b4439492e543f6e6dadd9136
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 19:46:43 2023 +0200
    
        wip
    
    commit 41d1ff581e15b7e2ebb72f5e80196122fae2ed85
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 19:41:00 2023 +0200
    
        wip
    
    commit 93821eafdad86d797ddcf9d82f11990c5b31bcc8
    Author: Alexey Evlampiev <alexey.evlampiev@gmail.com>
    Date:   Fri Jul 14 19:32:46 2023 +0200
    
        wip

---
+++ test/Solitons.Core.XUnitTest/Net/Http/HttpMessageSerializerTest.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Solitons.Net.Http;
+
+public sealed class HttpMessageSerializerTest
+{
+    [Fact]
+    public async Task SerializeDeserializeRequestTest()
+    {
+        // Arrange
+        var originalRequest = new HttpRequestMessage(HttpMethod.Get, "http://localhost");
+        originalRequest.Headers.Add("X-Test", "TestValue");
+        originalRequest.Content = new StringContent("Test Content");
+        originalRequest.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(originalRequest, stream);
+        stream.Position = 0;
+        var deserializedRequest = await HttpMessageSerializer.DeserializeRequestAsync(stream);
+
+        // Assert
+        Assert.Equal(originalRequest.Method, deserializedRequest.Method);
+        Assert.Equal(originalRequest.RequestUri, deserializedRequest.RequestUri);
+        Assert.Equal(originalRequest.Headers.ToString(), deserializedRequest.Headers.ToString());
+        Assert.Equal(await originalRequest.Content.ReadAsStringAsync(), await deserializedRequest.Content.ReadAsStringAsync());
+    }
+
+    [Fact]
+    public async Task SerializeDeserializeResponseTest()
+    {
+        // Arrange
+        var originalResponse = new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("Test Content"),
+            ReasonPhrase = "OK"
+        };
+        originalResponse.Headers.Add("X-Test", "TestValue");
+        originalResponse.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(originalResponse, stream);
+        stream.Position = 0;
+        var deserializedResponse = await HttpMessageSerializer.DeserializeResponseAsync(stream);
+
+        // Assert
+        Assert.Equal(originalResponse.StatusCode, deserializedResponse.StatusCode);
+        Assert.Equal(originalResponse.ReasonPhrase, deserializedResponse.ReasonPhrase);
+        Assert.Equal(originalResponse.Headers.ToString(), deserializedResponse.Headers.ToString());
+        var actualContent = await deserializedResponse.Content.ReadAsStringAsync();
+        Assert.Equal("Test Content", actualContent);
+    }
+
+    [Fact]
+    public async Task DeserializeEmptyRequestTest()
+    {
+        // Arrange
+        var emptyRequest = new HttpRequestMessage();
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(emptyRequest, stream);
+        stream.Position = 0;
+        var deserializedRequest = await HttpMessageSerializer.DeserializeRequestAsync(stream);
+
+        // Assert
+        Assert.Equal(emptyRequest.Method, deserializedRequest.Method);
+        Assert.Equal(emptyRequest.RequestUri, deserializedRequest.RequestUri);
+        Assert.Equal(emptyRequest.Headers.Count(), deserializedRequest.Headers.Count());
+        Assert.Null(deserializedRequest.Content);
+    }
+
+    [Fact]
+    public async Task DeserializeEmptyResponseTest()
+    {
+        // Arrange
+        var emptyResponse = new HttpResponseMessage();
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(emptyResponse, stream);
+        stream.Position = 0;
+        var deserializedResponse = await HttpMessageSerializer.DeserializeResponseAsync(stream);
+
+        // Assert
+        Assert.Equal(emptyResponse.StatusCode, deserializedResponse.StatusCode);
+        Assert.Equal(emptyResponse.ReasonPhrase, deserializedResponse.ReasonPhrase);
+        Assert.Equal(emptyResponse.Headers.Count(), deserializedResponse.Headers.Count());
+    }
+
+    [Fact]
+    public async Task SerializeRequestWithMultipleHeadersTest()
+    {
+        // Arrange
+        var originalRequest = new HttpRequestMessage(HttpMethod.Get, "http://localhost");
+        originalRequest.Headers.Add("X-Test1", new List<string> { "Value1", "Value2" });
+        originalRequest.Headers.Add("X-Test2", new List<string> { "Value3", "Value4" });
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(originalRequest, stream);
+        stream.Position = 0;
+        var deserializedRequest = await HttpMessageSerializer.DeserializeRequestAsync(stream);
+
+        // Assert
+        Assert.Equal(originalRequest.Headers.Count(), deserializedRequest.Headers.Count());
+        Assert.True(deserializedRequest.Headers.Contains("X-Test1"));
+        Assert.True(deserializedRequest.Headers.Contains("X-Test2"));
+    }
+
+    [Fact]
+    public async Task SerializeResponseWithMultipleHeadersTest()
+    {
+        // Arrange
+        var originalResponse = new HttpResponseMessage(HttpStatusCode.OK);
+        originalResponse.Headers.Add("X-Test1", new List<string> { "Value1", "Value2" });
+        originalResponse.Headers.Add("X-Test2", new List<string> { "Value3", "Value4" });
+        var stream = new MemoryStream();
+
+        // Act
+        await HttpMessageSerializer.SerializeAsync(originalResponse, stream);
+        stream.Position = 0;
+        var deserializedResponse = await HttpMessageSerializer.DeserializeResponseAsync(stream);
+
+        // Assert
+        Assert.Equal(originalResponse.Headers.Count(), deserializedResponse.Headers.Count());
+        Assert.True(deserializedResponse.Headers.Contains("X-Test1"));
+        Assert.True(deserializedResponse.Headers.Contains("X-Test2"));
+    }
+
+    [Fact]
+    public async Task DeserializeCorruptedStreamTest()
+    {
+        // Arrange
+        var stream = new MemoryStream();
+        var writer = new BinaryWriter(stream);
+        writer.Write("This is not a valid serialized HttpRequestMessage");
+        stream.Position = 0;
+
+        // Act & Assert
+        await Assert.ThrowsAsync<HttpMessageSerializationException>(() => HttpMessageSerializer.DeserializeRequestAsync(stream));
+    }
+}
