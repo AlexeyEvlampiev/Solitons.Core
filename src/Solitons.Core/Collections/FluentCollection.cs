5be08707c174ff181da65a73692fcd3b041007f6
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2023-10-22 14:56:08 +0200

    wip

--- src/Solitons.Core/Collections/FluentCollection.cs
+++ src/Solitons.Core/Collections/FluentCollection.cs
@@ -7,16 +7,113 @@ using System.Linq;
 namespace Solitons.Collections;
 
 /// <summary>
-/// A generic collection class that provides a fluent interface for manipulating collections of items of type T.
+/// Provides a set of static methods to create <see cref="FluentCollection{T}"/> instances.
+/// </summary>
+public static class FluentCollection
+{
+    /// <summary>
+    /// Creates a new <see cref="FluentCollection{T}"/> from the specified collection.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements in the collection.</typeparam>
+    /// <param name="collection">The collection from which to create a FluentCollection.</param>
+    /// <returns>A <see cref="FluentCollection{T}"/> containing the elements of the specified collection.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var list = new List<int> { 1, 2, 3 };
+    /// var fluentCollection = FluentCollection.Create(list);
+    /// ]]>
+    /// </code>
+    /// </example>
+    public static FluentCollection<T> Create<T>(IEnumerable<T> collection)
+    {
+        if (collection is FluentCollection<T> other)
+        {
+            return other;
+        }
+
+        return new FluentCollection<T>(collection);
+    }
+
+    /// <summary>
+    /// Creates a new <see cref="FluentCollection{T}"/> containing a single item.
+    /// </summary>
+    /// <typeparam name="T">The type of the item.</typeparam>
+    /// <param name="item">The item to include in the collection.</param>
+    /// <returns>A <see cref="FluentCollection{T}"/> containing the specified item.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create(42);
+    /// ]]>
+    /// </code>
+    /// </example>
+    public static FluentCollection<T> Create<T>(T item) => new(FluentArray.Create(item));
+
+    /// <summary>
+    /// Creates a new <see cref="FluentCollection{T}"/> containing two items.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    /// <param name="item1">The first item to include in the collection.</param>
+    /// <param name="item2">The second item to include in the collection.</param>
+    /// <returns>A <see cref="FluentCollection{T}"/> containing the specified items.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create(42, 43);
+    /// ]]>
+    /// </code>
+    /// </example>
+    public static FluentCollection<T> Create<T>(T item1, T item2) => new(FluentArray.Create(item1, item2));
+
+    /// <summary>
+    /// Creates a new <see cref="FluentCollection{T}"/> containing three items.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    /// <param name="item1">The first item to include in the collection.</param>
+    /// <param name="item2">The second item to include in the collection.</param>
+    /// <param name="item3">The third item to include in the collection.</param>
+    /// <returns>A <see cref="FluentCollection{T}"/> containing the specified items.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create(42, 43, 44);
+    /// ]]>
+    /// </code>
+    /// </example>
+    public static FluentCollection<T> Create<T>(T item1, T item2, T item3) => new(FluentArray.Create(item1, item2, item3));
+
+    /// <summary>
+    /// Creates a new <see cref="FluentCollection{T}"/> containing the specified items.
+    /// </summary>
+    /// <typeparam name="T">The type of the items.</typeparam>
+    /// <param name="items">The items to include in the collection.</param>
+    /// <returns>A <see cref="FluentCollection{T}"/> containing the specified items.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create(42, 43, 44, 45);
+    /// ]]>
+    /// </code>
+    /// </example>
+    public static FluentCollection<T> Create<T>(params T[] items) => new(FluentArray.Create(items));
+}
+
+/// <summary>
+/// A generic collection class that provides a fluent interface for manipulating collections of items of type <typeparamref name="T"/>.
 /// </summary>
 /// <typeparam name="T">The type of item stored in the collection.</typeparam>
+/// <remarks>
+/// The FluentCollection class is a wrapper around a collection that provides a fluent interface for performing operations on the collection.
+/// Each method returns the FluentCollection object, allowing for method chaining.
+/// </remarks>
 public sealed class FluentCollection<T> : ICollection<T>
 {
     [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
     private readonly ICollection<T> _collection;
 
     /// <summary>
-    /// Initializes a new instance of the FluentCollection class that contains elements copied from the specified enumerable.
+    /// Initializes a new instance of the <see cref="FluentCollection{T}"/> class that contains elements copied from the specified enumerable.
     /// </summary>
     /// <param name="enumerable">The enumerable to copy elements from.</param>
     /// <exception cref="ArgumentNullException">If the enumerable is null.</exception>
@@ -42,10 +139,19 @@ public sealed class FluentCollection<T> : ICollection<T>
     void ICollection<T>.Add(T item) => _collection.Add(item);
 
     /// <summary>
-    /// Adds an element to the FluentCollection.
+    /// Adds an element to the <see cref="FluentCollection{T}"/>.
     /// </summary>
     /// <param name="item">The item to add to the collection.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection
+    ///     .Create<int>()
+    ///     .Add(42);
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> Add(T item)
     {
@@ -54,11 +160,19 @@ public sealed class FluentCollection<T> : ICollection<T>
     }
 
     /// <summary>
-    /// Adds the elements of the specified collection to the FluentCollection.
+    /// Adds the elements of the specified collection to the <see cref="FluentCollection{T}"/>.
     /// </summary>
     /// <param name="collection">The collection to add elements from.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
     /// <exception cref="ArgumentNullException">If the collection is null.</exception>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>();
+    /// fluentCollection.AddRange(new List<int> { 1, 2, 3 });
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> AddRange(IEnumerable<T> collection)
     {
@@ -77,9 +191,17 @@ public sealed class FluentCollection<T> : ICollection<T>
 
 
     /// <summary>
-    /// Removes all elements from the FluentCollection.
+    /// Removes all elements from the <see cref="FluentCollection{T}"/>.
     /// </summary>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3);
+    /// fluentCollection.Clear();
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> Clear()
     {
@@ -97,11 +219,20 @@ public sealed class FluentCollection<T> : ICollection<T>
     void ICollection<T>.CopyTo(T[] array, int arrayIndex) => _collection.CopyTo(array, arrayIndex);
 
     /// <summary>
-    /// Copies the elements of the FluentCollection to an array, starting at a particular array index.
+    /// Copies the elements of the <see cref="FluentCollection{T}"/> to an array, starting at a particular array index.
     /// </summary>
     /// <param name="array">The array to copy elements to.</param>
     /// <param name="arrayIndex">The index in the array to start copying elements.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3);
+    /// var array = new int[3];
+    /// fluentCollection.CopyTo(array, 0);
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> CopyTo(T[] array, int arrayIndex)
     {
@@ -115,11 +246,20 @@ public sealed class FluentCollection<T> : ICollection<T>
     public bool Remove(T item) => _collection.Remove(item);
 
     /// <summary>
-    /// Removes the first occurrence of a specific object from the FluentCollection.
+    /// Removes the first occurrence of a specific object from the <see cref="FluentCollection{T}"/>.
     /// </summary>
     /// <param name="item">The item to remove from the collection.</param>
     /// <param name="removed">True if the item was removed; otherwise, false.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3);
+    /// bool removed;
+    /// fluentCollection.Remove(2, out removed);
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> Remove(T item, out bool removed)
     {
@@ -128,11 +268,19 @@ public sealed class FluentCollection<T> : ICollection<T>
     }
 
     /// <summary>
-    /// Removes all elements that match the conditions defined by the specified predicate from the FluentCollection.
+    /// Removes all elements that match the conditions defined by the specified predicate from the <see cref="FluentCollection{T}"/>.
     /// </summary>
     /// <param name="selector">The predicate that defines the conditions of the elements to remove.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
     /// <exception cref="ArgumentNullException">If the selector is null.</exception>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3, 4, 5);
+    /// fluentCollection.Remove(item => item % 2 == 0);
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> Remove(Func<T, bool> selector)
     {
@@ -145,12 +293,21 @@ public sealed class FluentCollection<T> : ICollection<T>
     }
 
     /// <summary>
-    /// Removes all elements that match the conditions defined by the specified predicate from the FluentCollection, and returns the number of elements removed.
+    /// Removes all elements that match the conditions defined by the specified predicate from the <see cref="FluentCollection{T}"/>, and returns the number of elements removed.
     /// </summary>
     /// <param name="selector">The predicate that defines the conditions of the elements to remove.</param>
     /// <param name="removed">The number of elements removed.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
     /// <exception cref="ArgumentNullException">If the selector is null.</exception>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3, 4, 5);
+    /// int removed;
+    /// fluentCollection.Remove(item => item % 2 == 0, out removed);
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> Remove(Func<T, bool> selector, out int removed)
     {
@@ -173,11 +330,20 @@ public sealed class FluentCollection<T> : ICollection<T>
     public bool IsReadOnly => _collection.IsReadOnly;
 
     /// <summary>
-    /// Performs the specified action on each element of the FluentCollection.
+    /// Performs the specified action on each element of the <see cref="FluentCollection{T}"/>.
     /// </summary>
     /// <param name="action">The action to perform on each element of the collection.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
     /// <exception cref="ArgumentNullException">If the action is null.</exception>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3);
+    /// fluentCollection.ForEach(item => Console
+    /// WriteLine(item));
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> ForEach(Action<T> action)
     {
@@ -191,11 +357,19 @@ public sealed class FluentCollection<T> : ICollection<T>
     }
 
     /// <summary>
-    /// Performs the specified action on each element of the FluentCollection, with an index parameter.
+    /// Performs the specified action on each element of the <see cref="FluentCollection{T}"/>, with an index parameter.
     /// </summary>
     /// <param name="action">The action to perform on each element of the collection.</param>
-    /// <returns>The FluentCollection object, allowing for method chaining.</returns>
+    /// <returns>The <see cref="FluentCollection{T}"/> object, allowing for method chaining.</returns>
     /// <exception cref="ArgumentNullException">If the action is null.</exception>
+    /// <example>
+    /// <code>
+    /// <![CDATA[
+    /// var fluentCollection = FluentCollection.Create<int>(1, 2, 3);
+    /// fluentCollection.ForEach((item, index) => Console.WriteLine($"{index}: {item}"));
+    /// ]]>
+    /// </code>
+    /// </example>
     [DebuggerStepThrough]
     public FluentCollection<T> ForEach(Action<T, int> action)
     {
