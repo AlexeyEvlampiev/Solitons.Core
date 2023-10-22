91e2589ca03a0a2c11b1c6bc2c524c166689ad79
Alexey Evlampiev <alexey.evlampiev@gmail.com>
2023-10-22 13:19:26 +0200

    wip

--- src/Solitons.Core/Collections/DictionaryKeyAttribute.cs
+++ src/Solitons.Core/Collections/DictionaryKeyAttribute.cs
@@ -1,17 +1,72 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Solitons.Collections;
 
 /// <summary>
-/// Indicates that a property is the key for a dictionary-like data structure.
+/// The <c>DictionaryKeyAttribute</c> is used to specify a custom key name for a property 
+/// when building a key-value pair collection from a class. 
+/// By default, the property name is used as the key, but applying this attribute 
+/// allows for a custom key name to be defined.
 /// </summary>
+/// <example>
+/// Here is an example of how to use the <c>DictionaryKeyAttribute</c>:
+/// <code><![CDATA[
+/// namespace Solitons.Examples
+/// {
+///     using Solitons.Collections;
+///     using System;
+///     using System.Collections.Generic;
+///     using System.Reflection;
+///
+///     public class Person
+///     {
+///         [DictionaryKey("First_Name")]
+///         public string FirstName { get; set; }
+/// 
+///         [DictionaryKey("Last_Name")]
+///         public string LastName { get; set; }
+///     }
+/// 
+///     public class Program
+///     {
+///         public static void Main(string[] args)
+///         {
+///             var person = new Person { FirstName = "John", LastName = "Doe" };
+///             var dictionary = GetDictionary(person);
+/// 
+///             foreach (var kvp in dictionary)
+///             {
+///                 Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+///             }
+///         }
+/// 
+///         public static Dictionary<string, object> GetDictionary(object obj)
+///         {
+///             var dictionary = new Dictionary<string, object>();
+///             var properties = obj.GetType().GetProperties();
+/// 
+///             foreach (var property in properties)
+///             {
+///                 var key = DictionaryKeyAttribute.ResolveKeyName(property);
+///                 var value = property.GetValue(obj);
+///                 dictionary.Add(key, value);
+///             }
+/// 
+///             return dictionary;
+///         }
+///     }
+/// }
+/// ]]></code>
+/// </example>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class DictionaryKeyAttribute : Attribute
 {
     /// <summary>
-    /// Initializes a new instance of the DictionaryKeyAttribute class with the specified key name.
+    /// Initializes a new instance of the <see cref="DictionaryKeyAttribute"/> class with the specified key name.
     /// </summary>
-    /// <param name="name">The name of the key for the property.</param>
+    /// <param name="name">The custom name of the key for the property.</param>
     /// <exception cref="ArgumentNullException">Thrown when the specified name is null or empty.</exception>
     public DictionaryKeyAttribute(string name)
     {
@@ -19,7 +74,21 @@ public sealed class DictionaryKeyAttribute : Attribute
     }
 
     /// <summary>
-    /// Gets the name of the key for the property.
+    /// Gets the custom name of the key for the property.
     /// </summary>
     public string Name { get; }
+
+    /// <summary>
+    /// Retrieves the custom key name for the specified property, 
+    /// or falls back to the property name if <see cref="DictionaryKeyAttribute"/> is not applied.
+    /// </summary>
+    /// <param name="property">The property for which to retrieve the key name.</param>
+    /// <returns>The custom key name or the property name.</returns>
+    [DebuggerNonUserCode]
+    public static string ResolveKeyName(PropertyInfo property)
+    {
+        var attribute = property
+            .GetCustomAttribute<DictionaryKeyAttribute>()?.Name;
+        return attribute.DefaultIfNullOrWhiteSpace(property.Name)!;
+    }
 }
---
+++ test/Solitons.Core.XUnitTest/Collections/DictionaryKeyAttribute_ResolveKeyName_Should.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+
+namespace Solitons.Collections;
+
+// ReSharper disable once InconsistentNaming
+public sealed class DictionaryKeyAttribute_ResolveKeyName_Should
+{
+    [Fact]
+    public void ReturnPropertyName_WhenAttributeIsNotApplied()
+    {
+        // Arrange
+        var person = new Person { FirstName = "John", LastName = "Doe" };
+        var propertyInfo = person.GetType().GetProperty(nameof(Person.LastName));
+
+        // Act
+        var keyName = DictionaryKeyAttribute.ResolveKeyName(propertyInfo);
+
+        // Assert
+        Assert.Equal(nameof(Person.LastName), keyName);
+    }
+
+    [Fact]
+    public void ResolveKeyName_ShouldReturnCustomKeyName_WhenAttributeIsApplied()
+    {
+        // Arrange
+        var person = new Person { FirstName = "John", LastName = "Doe" };
+        var propertyInfo = person.GetType().GetProperty(nameof(Person.FirstName));
+
+        // Act
+        var keyName = DictionaryKeyAttribute.ResolveKeyName(propertyInfo);
+
+        // Assert
+        Assert.Equal("27b90765-7daf-4807-a562-030ff251e15f", keyName);
+    }
+
+
+    // Assuming the Person class is defined as below
+    public class Person
+    {
+        [DictionaryKey("27b90765-7daf-4807-a562-030ff251e15f")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
